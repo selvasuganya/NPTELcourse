f5d48ff8a56eb92fa9bda9c2b10fd8147f7e5033
selvasuganya <79531410+selvasuganya@users.noreply.github.com>
2023-08-31 15:53:03 +0530

    Create Sumofseries.c

---
+++ WEEKS/Sumofseries.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+int N;
+float sum = 0.0;
+scanf("%d",&N); /*Read the value of N from test cases provided*/
+
+/* Complete the program. Please use the printf statement given below:
+
+printf("Sum of the series is: %.2f\n",sum);
+
+*/
+float i;
+for(i=1;i<=N; i++){
+  sum += (1/i);
+}
+printf("Sum of the series is: %.2f",sum);
+}
